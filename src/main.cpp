48c7955b0102cecea06b0842a5a6bfa242942401
Logan Crocker <lcroc001@ucr.edu>
2016-05-12 16:38:31 -0700

    Added includes

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <netdb.h>
+#include <pwd.h>
+#include <sys/socket.h>
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
+using namespace boost::algorithm;
+
+int main () {
+
+    return 0;
+}

91ec544734f3960fe64ac6633c2ae582b3dbf450
arizvi002 <arizvi.1993@gmail.com>
2016-05-12 17:33:17 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,3 @@
-//
-//  AHDFH.cpp
-//  CS100
-//
-//  Created by Ammar Rizvi on 5/12/16.
-//  Copyright © 2016 Ammar Rizvi. All rights reserved.
-//
-
-#include "AHDFH.hpp"
 #include <iostream>
 #include <vector>
 #include <list>
@@ -19,18 +10,25 @@
 #include <netdb.h>
 #include <pwd.h>
 #include <sys/socket.h>
-//#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string.hpp>
 
 using namespace std;
-//using namespace boost::algorithm;
+using namespace boost::algorithm;
 
-
-// Virtual Base Class
 class Base{
-public:
-    Base(){};
-    // Virtual Function that will be inherited
-    virtual bool evaluate() = 0;
+    public:
+        Base(){};
+        virtual bool evaluate() = 0;
+};
+
+class Command: public Base{
+    private:
+        vector<string> commandVec;
+    public:
+        Command(vector<string>s){
+            commandVec = s;
+        }
+  
 };
 
 int main () {
@@ -44,8 +42,8 @@ int main () {
         cout << "[" << login << "@" << hostname << "] $ ";
         getline(cin, initialCommand);
         trim(initialCommand);
-        
+
     }
-    
+
     return 0;
 }

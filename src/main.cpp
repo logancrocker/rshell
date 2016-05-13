7936ed4261c8787c46bceb028e7b1925b9426f8f
arizvi002 <arizvi.1993@gmail.com>
2016-05-12 17:42:51 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,23 +17,32 @@ using namespace boost::algorithm;
 
 //Virtual Base Class
 class Base{
-    public:
-        Base(){};
-        //Function Inherited by each class
-        virtual bool evaluate() = 0;
+public:
+    Base(){};
+    //Function Inherited by each class
+    virtual bool evaluate() = 0;
 };
 
 // Command Class that each command will inherit from
 class Command: public Base{
-    private:
-    //Vector of commands 
-        vector<string> commandVec;
-    public:
+private:
+    //Vector of commands
+    vector<string> commandVec;
+public:
     //Contructor to take in vector and set it to commands vectors
-        Command(vector<string>s){
-            commandVec = s;
-        }
-  
+    Command(vector<string>s){
+        commandVec = s;
+    }
+    
+};
+
+class Connectors:public Base{
+private:
+    Connectors(){};
+    
+protected:
+    bool leftCommand;
+    Base* rightCommand;
 };
 
 int main () {
@@ -48,8 +57,8 @@ int main () {
         cout << "[" << login << "@" << hostname << "] $ ";
         getline(cin, initialCommand);
         trim(initialCommand);
-
+        
     }
-
+    
     return 0;
 }
